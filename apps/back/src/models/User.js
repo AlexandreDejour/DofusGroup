b64ad4c1f212615b9e622758644a8f612b3dd711
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-05-23 13:10:24 +0200

    Create User model

---
+++ apps/back/src/models/User.js
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from "Sequelize";
+
+import { client } from "../client/client.js";
+
+class User extends Model {};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            autoIncrementIdentity: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        mail: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        avatar: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize: client
+    }
+);

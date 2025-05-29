b75da87dc7b740a42e45ba3ecd06fbadd7b7debc
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-05-29 12:48:08 +0200

    Insert errorHandler in index.js

--- apps/back/index.js
+++ apps/back/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 import { app } from "./src/app/app.js";
 import { router } from "./src/router/index.js";
+import { notFound, errorHandler } from "./src/middlewares/errorHandler.js";
 
 import { sync } from "./src/database/sync.js";
 import { seeding } from "./src/database/seeding.js";
@@ -11,6 +12,9 @@ try {
     await seeding();
 
     app.use(router);
+
+    app.use(notFound);
+    app.use(errorHandler);
     
     app.listen(app.get("port"), () => {
         console.info(`Listening on ${app.get("base_url")}:${app.get("port")} 🚀.`);

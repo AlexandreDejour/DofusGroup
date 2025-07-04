56f4d5ec812097b021b31248cf71c3de57bdbe1a
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-07-04 12:16:33 +0200

    Create joi schema validator

--- apps/back/src/api/router/characterRouter.ts
+++ apps/back/src/api/router/characterRouter.ts
@@ -2,7 +2,12 @@ import { Router } from "express";
 
 import validateUUID from "../../middlewares/utils/validateUUID.js";
 import htmlSanitizer from "../../middlewares/utils/htmlSanitizer.js";
+import validateSchema from "../../middlewares/joi/validateSchema.js";
 import { CharacterController } from "../controllers/characterController.js";
+import {
+  createCharacterSchema,
+  updateCharacterSchema,
+} from "../../middlewares/joi/schemas/character.js";
 
 const characterRouter: Router = Router();
 const controller: CharacterController = new CharacterController();
@@ -27,6 +32,7 @@ characterRouter.post(
   "/user/:userId/character",
   validateUUID,
   htmlSanitizer,
+  validateSchema(createCharacterSchema),
   (req, res, next) => {
     controller.post(req, res, next);
   },
@@ -37,9 +43,14 @@ characterRouter
   .get(validateUUID, (req, res, next) => {
     controller.getOneByUserId(req, res, next);
   })
-  .patch(validateUUID, htmlSanitizer, (req, res, next) => {
-    controller.update(req, res, next);
-  })
+  .patch(
+    validateUUID,
+    htmlSanitizer,
+    validateSchema(updateCharacterSchema),
+    (req, res, next) => {
+      controller.update(req, res, next);
+    },
+  )
   .delete(validateUUID, (req, res, next) => {
     controller.delete(req, res, next);
   });

2317b1d674ce600a32d5ca696c4eb694c254c726
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-07-11 16:55:19 +0200

    Create serverRouter's tests

--- apps/back/src/api/router/_tests_/breedRouter.test.ts
+++ apps/back/src/api/router/_tests_/breedRouter.test.ts
@@ -3,28 +3,28 @@ import { vi, describe, it, expect, beforeEach } from "vitest";
 import status from "http-status";
 
 import { setup, receivedReq } from "./mock-tools.js";
-import { createServerRouter } from "../serverRouter.js";
-import { ServerController } from "../../controllers/serverController.js";
-import { ServerRepository } from "../../../middlewares/repository/serverRepository.js";
+import { createBreedRouter } from "../breedRouter.js";
+import { BreedController } from "../../controllers/breedController.js";
+import { BreedRepository } from "../../../middlewares/repository/breedRepository.js";
 
-describe("serverRouter", () => {
-  const repository = {} as ServerRepository;
-  const controller = new ServerController(repository);
+describe("breedRouter", () => {
+  const repository = {} as BreedRepository;
+  const controller = new BreedController(repository);
   let app: ReturnType<typeof setup.App>;
 
   beforeEach(() => {
     vi.clearAllMocks();
-    app = setup.App(controller, createServerRouter);
+    app = setup.App(controller, createBreedRouter);
   });
 
-  const serverId = "e5b25782-deea-4f73-b8f0-47b7e0c99e67";
+  const breedId = "e5b25782-deea-4f73-b8f0-47b7e0c99e67";
 
-  describe("GET /servers", () => {
-    it("Propagate request to serverController.getAll", async () => {
+  describe("GET /breeds", () => {
+    it("Propagate request to breedController.getAll", async () => {
       //GIVEN
       controller.getAll = setup.mockSucessCall(status.OK);
       //WHEN
-      const res = await request(app).get("/servers");
+      const res = await request(app).get("/breeds");
       //THEN
       expect(controller.getAll).toHaveBeenCalled();
       expect(res.status).toBe(status.OK);
@@ -34,7 +34,7 @@ describe("serverRouter", () => {
     it("Next is called at end route.", async () => {
       controller.getAll = setup.mockNextCall();
 
-      const res = await request(app).get("/servers");
+      const res = await request(app).get("/breeds");
 
       expect(controller.getAll).toHaveBeenCalled();
       expect(res.status).toBe(status.NOT_FOUND);
@@ -42,15 +42,15 @@ describe("serverRouter", () => {
     });
   });
 
-  describe("GET /server/:serverId", () => {
-    it("Propagate request to serverController.getOne", async () => {
+  describe("GET /breed/:breedId", () => {
+    it("Propagate request to breedController.getOne", async () => {
       //GIVEN
       controller.getOne = setup.mockSucessCall(status.OK);
       //WHEN
-      const res = await request(app).get(`/server/${serverId}`);
+      const res = await request(app).get(`/breed/${breedId}`);
       //THEN
       expect(controller.getOne).toHaveBeenCalled();
-      expect(receivedReq?.params.serverId).toBe(serverId);
+      expect(receivedReq?.params.breedId).toBe(breedId);
       expect(res.status).toBe(status.OK);
       expect(res.body).toBe("Success!");
     });
@@ -58,7 +58,7 @@ describe("serverRouter", () => {
     it("Next is called at end route.", async () => {
       controller.getOne = setup.mockNextCall();
 
-      const res = await request(app).get(`/server/${serverId}`);
+      const res = await request(app).get(`/breed/${breedId}`);
 
       expect(controller.getOne).toHaveBeenCalled();
       expect(res.status).toBe(status.NOT_FOUND);
@@ -66,7 +66,7 @@ describe("serverRouter", () => {
     });
 
     it("Excluded bad request when id isn't a UUID.", async () => {
-      const res = await request(app).get("/server/toto");
+      const res = await request(app).get("/breed/toto");
 
       expect(controller.getOne).not.toHaveBeenCalled();
       expect(res.status).toBe(status.BAD_REQUEST);

1ffc5269588a2abbeb481e9c375f726d0dbe7e68
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-07-13 15:13:45 +0200

    Create new user types

--- apps/back/src/types/user.ts
+++ apps/back/src/types/user.ts
@@ -1,4 +1,17 @@
+import { Event } from "./event.js";
+import { Character } from "./character.js";
+
 export type User = {
   id: string;
   username: string;
+};
+
+export type UserEnriched = User & {
+  events?: Event[];
+  characters?: Character[];
+};
+
+export type UserBodyData = Omit<User, "id"> & {
+  password: string;
+  mail: string;
 };

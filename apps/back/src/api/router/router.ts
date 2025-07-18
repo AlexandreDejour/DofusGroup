7680954d183ad0a540ce85f1dd08dc1b0d9a2b8d
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-07-18 15:26:28 +0200

    Import event router in main router

--- apps/back/src/api/router/router.ts
+++ apps/back/src/api/router/router.ts
@@ -11,6 +11,9 @@ import { TagRepository } from "../../middlewares/repository/tagRepository.js";
 import { createUserRouter } from "./userRouter.js";
 import { UserController } from "../controllers/userController.js";
 import { UserRepository } from "../../middlewares/repository/userRepository.js";
+import { createEventRouter } from "./eventRouter.js";
+import { EventController } from "../controllers/eventController.js";
+import { EventRepository } from "../../middlewares/repository/eventRepository.js";
 import { createBreedRouter } from "./breedRouter.js";
 import { BreedController } from "../controllers/breedController.js";
 import { BreedRepository } from "../../middlewares/repository/breedRepository.js";
@@ -28,6 +31,7 @@ initAssociations(models);
 
 const tagController = new TagController(new TagRepository());
 const userController = new UserController(new UserRepository());
+const eventController = new EventController(new EventRepository());
 const breedController = new BreedController(new BreedRepository());
 const serverController = new ServerController(new ServerRepository());
 const characterController = new CharacterController(new CharacterRepository());
@@ -40,6 +44,7 @@ router.get("/", (_req: Request, res: Response) => {
 
 router.use(createTagRouter(tagController));
 router.use(createUserRouter(userController, dataEncryptionService));
+router.use(createEventRouter(eventController));
 router.use(createBreedRouter(breedController));
 router.use(createServerRouter(serverController));
 router.use(createCharacterRouter(characterController));

7c0e739c579fb70ead003e8756ae3a1dc76e208e
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-07-20 11:44:12 +0200

    Import error handler middlewares in index

--- apps/back/index.ts
+++ apps/back/index.ts
@@ -3,6 +3,8 @@ import { Express } from "express";
 
 import { Config } from "./src/config/config.js";
 import router from "./src/api/router/router.js";
+import { notFound } from "./src/middlewares/utils/errorHandler.js";
+import { errorHandler } from "./src/middlewares/utils/errorHandler.js";
 
 const app: Express = express();
 const config = Config.getInstance();
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.info(`Listening on ${config.baseUrl}:${config.port}`);

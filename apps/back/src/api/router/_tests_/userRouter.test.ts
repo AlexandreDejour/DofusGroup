2b2b1cff6dce83d0014f635fa333c6e266a14a93
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-07-21 19:16:44 +0200

    Fix userRouter's tests

--- apps/back/src/api/router/_tests_/userRouter.test.ts
+++ apps/back/src/api/router/_tests_/userRouter.test.ts
@@ -49,90 +49,90 @@ describe("userRouter", () => {
       expect(res.status).toBe(status.NOT_FOUND);
       expect(res.body).toEqual({ called: "next" });
     });
+  });
 
-    describe("GET /user/:userId", () => {
-      it("Propagate request to userController.getOne", async () => {
-        //GIVEN
-        controller.getOne = setup.mockSucessCall(status.OK);
-        //WHEN
-        const res = await request(app).get(`/user/${userId}`);
-        //THEN
-        expect(controller.getOne).toHaveBeenCalled();
-        expect(receivedReq?.params.userId).toBe(userId);
-        expect(res.status).toBe(status.OK);
-        expect(res.body).toBe("Success!");
-      });
-
-      it("Next is called at end route.", async () => {
-        controller.getOne = setup.mockNextCall();
-
-        const res = await request(app).get(`/user/${userId}`);
-
-        expect(controller.getOne).toHaveBeenCalled();
-        expect(res.status).toBe(status.NOT_FOUND);
-        expect(res.body).toEqual({ called: "next" });
-      });
-
-      it("Excluded bad request when id isn't a UUID.", async () => {
-        const res = await request(app).get("/user/1234");
-
-        expect(controller.getOne).not.toHaveBeenCalled();
-        expect(res.status).toBe(status.BAD_REQUEST);
-      });
+  describe("GET /user/:userId", () => {
+    it("Propagate request to userController.getOne", async () => {
+      //GIVEN
+      controller.getOne = setup.mockSucessCall(status.OK);
+      //WHEN
+      const res = await request(app).get(`/user/${userId}`);
+      //THEN
+      expect(controller.getOne).toHaveBeenCalled();
+      expect(receivedReq?.params.userId).toBe(userId);
+      expect(res.status).toBe(status.OK);
+      expect(res.body).toBe("Success!");
     });
 
-    describe("GET /users/enriched", () => {
-      it("Propagate request to userController.getAllEnriched", async () => {
-        //GIVEN
-        controller.getAllEnriched = setup.mockSucessCall(status.OK);
-        //WHEN
-        const res = await request(app).get(`/users/enriched`);
-        //THEN
-        expect(controller.getAllEnriched).toHaveBeenCalled();
-        expect(res.status).toBe(status.OK);
-        expect(res.body).toBe("Success!");
-      });
+    it("Next is called at end route.", async () => {
+      controller.getOne = setup.mockNextCall();
 
-      it("Next is called at end route.", async () => {
-        controller.getAllEnriched = setup.mockNextCall();
+      const res = await request(app).get(`/user/${userId}`);
 
-        const res = await request(app).get(`/users/enriched`);
-
-        expect(controller.getAllEnriched).toHaveBeenCalled();
-        expect(res.status).toBe(status.NOT_FOUND);
-        expect(res.body).toEqual({ called: "next" });
-      });
+      expect(controller.getOne).toHaveBeenCalled();
+      expect(res.status).toBe(status.NOT_FOUND);
+      expect(res.body).toEqual({ called: "next" });
     });
 
-    describe("GET /user/:userId/enriched", () => {
-      it("Propagate request to userController.getOneEnriched", async () => {
-        //GIVEN
-        controller.getOneEnriched = setup.mockSucessCall(status.OK);
-        //WHEN
-        const res = await request(app).get(`/user/${userId}/enriched`);
-        //THEN
-        expect(controller.getOneEnriched).toHaveBeenCalled();
-        expect(receivedReq?.params.userId).toBe(userId);
-        expect(res.status).toBe(status.OK);
-        expect(res.body).toBe("Success!");
-      });
+    it("Excluded bad request when id isn't a UUID.", async () => {
+      const res = await request(app).get("/user/1234");
 
-      it("Next is called at end route.", async () => {
-        controller.getOneEnriched = setup.mockNextCall();
+      expect(controller.getOne).not.toHaveBeenCalled();
+      expect(res.status).toBe(status.BAD_REQUEST);
+    });
+  });
 
-        const res = await request(app).get(`/user/${userId}/enriched`);
+  describe("GET /users/enriched", () => {
+    it("Propagate request to userController.getAllEnriched", async () => {
+      //GIVEN
+      controller.getAllEnriched = setup.mockSucessCall(status.OK);
+      //WHEN
+      const res = await request(app).get(`/users/enriched`);
+      //THEN
+      expect(controller.getAllEnriched).toHaveBeenCalled();
+      expect(res.status).toBe(status.OK);
+      expect(res.body).toBe("Success!");
+    });
 
-        expect(controller.getOneEnriched).toHaveBeenCalled();
-        expect(res.status).toBe(status.NOT_FOUND);
-        expect(res.body).toEqual({ called: "next" });
-      });
+    it("Next is called at end route.", async () => {
+      controller.getAllEnriched = setup.mockNextCall();
 
-      it("Excluded bad request when id isn't a UUID.", async () => {
-        const res = await request(app).get("/user/1234/enriched");
+      const res = await request(app).get(`/users/enriched`);
 
-        expect(controller.getOneEnriched).not.toHaveBeenCalled();
-        expect(res.status).toBe(status.BAD_REQUEST);
-      });
+      expect(controller.getAllEnriched).toHaveBeenCalled();
+      expect(res.status).toBe(status.NOT_FOUND);
+      expect(res.body).toEqual({ called: "next" });
+    });
+  });
+
+  describe("GET /user/:userId/enriched", () => {
+    it("Propagate request to userController.getOneEnriched", async () => {
+      //GIVEN
+      controller.getOneEnriched = setup.mockSucessCall(status.OK);
+      //WHEN
+      const res = await request(app).get(`/user/${userId}/enriched`);
+      //THEN
+      expect(controller.getOneEnriched).toHaveBeenCalled();
+      expect(receivedReq?.params.userId).toBe(userId);
+      expect(res.status).toBe(status.OK);
+      expect(res.body).toBe("Success!");
+    });
+
+    it("Next is called at end route.", async () => {
+      controller.getOneEnriched = setup.mockNextCall();
+
+      const res = await request(app).get(`/user/${userId}/enriched`);
+
+      expect(controller.getOneEnriched).toHaveBeenCalled();
+      expect(res.status).toBe(status.NOT_FOUND);
+      expect(res.body).toEqual({ called: "next" });
+    });
+
+    it("Excluded bad request when id isn't a UUID.", async () => {
+      const res = await request(app).get("/user/1234/enriched");
+
+      expect(controller.getOneEnriched).not.toHaveBeenCalled();
+      expect(res.status).toBe(status.BAD_REQUEST);
     });
   });
 

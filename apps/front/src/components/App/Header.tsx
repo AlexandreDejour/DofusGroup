ce9fbf6a011c1a06303696788949ade30471229c
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-08-08 13:12:18 +0200

    reset CSS properties

---
+++ apps/front/src/components/App/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.scss";
+
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>DofusGroup</h1>
+      <img src="" alt="" />
+    </header>
+  );
+}

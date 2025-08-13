88cc945a9f12db03e3abd5e6a02fe42331b3542a
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-08-13 18:46:31 +0200

    Add formular management in modalContext

--- apps/front/src/contexts/modalContext.tsx
+++ apps/front/src/contexts/modalContext.tsx
@@ -1,12 +1,25 @@
 import React, { createContext, useContext, useState } from "react";
+import DOMPurify from "dompurify";
+
+import { Config } from "../config/config";
+import { ApiClient } from "../services/client";
+import { AuthService } from "../services/api/authService";
+import formDataToObject from "./utils/formDataToObject";
+import { RegisterForm } from "../types/form";
+
+const config = Config.getInstance();
+const axios = new ApiClient(config.baseUrl);
+const authService = new AuthService(axios);
 
 interface ModalContextType {
   isOpen: boolean;
-  modalType: string | null; // ex: "signup", "login", "profileEdit", etc.
+  modalType: string | null; // ex: "register", "login", "newEvent", etc.
+  formData: FormData;
+  resetForm: React.Dispatch<React.SetStateAction<FormData>>;
   error: string | null;
+  setError: (message: string | null) => void;
   openModal: (type: string) => void;
   closeModal: () => void;
-  setError: (message: string | null) => void;
 }
 
 interface ModalProviderProps {
@@ -18,7 +31,45 @@ const ModalContext = createContext<ModalContextType | null>(null);
 export default function ModalProvider({ children }: ModalProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [modalType, setModalType] = useState<string | null>(null);
+  const [formData, setFormData] = useState<FormData>(new FormData());
   const [error, setError] = useState<string | null>(null);
+
+  async function handleSubmit(event: Event, formData: FormData) {
+    event.preventDefault();
+
+    try {
+      const rawData = Object.fromEntries(formData);
+
+      const data = Object.fromEntries(
+        Object.entries(rawData).map(([key, value]) => [
+          key,
+          DOMPurify.sanitize(value as string),
+        ]),
+      );
+
+      if (modalType === "register") {
+        const data = formDataToObject<RegisterForm>(formData, [
+          "username",
+          "mail",
+          "password",
+          "confirmPassword",
+        ]);
+        const response = authService.register(data);
+      }
+
+      setError(null);
+      console.log(data);
+      closeModal();
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("Une erreur est survenue");
+      }
+    }
+  }
+
+  const resetForm = () => setFormData(new FormData());
 
   const openModal = (type: string) => {
     setModalType(type);
@@ -34,10 +85,12 @@ export default function ModalProvider({ children }: ModalProviderProps) {
   const contextValues: ModalContextType = {
     isOpen,
     modalType,
+    formData,
+    resetForm,
     error,
+    setError,
     openModal,
     closeModal,
-    setError,
   };
 
   return (

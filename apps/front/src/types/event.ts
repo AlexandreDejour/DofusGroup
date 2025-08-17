4d0de58f837f0ee13ba39c3271ee58617751490f
AlexandreDejour <124265195+AlexandreDejour@users.noreply.github.com>
2025-08-17 20:21:10 +0200

    Create test for untested components (#24)

--- apps/front/src/types/event.ts
+++ apps/front/src/types/event.ts
@@ -7,9 +7,9 @@ export type Event = {
   title: string;
   date: Date;
   duration: number;
-  area: string;
-  sub_area: string;
-  donjon_name: string;
+  area?: string;
+  sub_area?: string;
+  donjon_name?: string;
   description?: string;
   max_players: number;
   status: string;

33317dd44a1988bc118a13784a0116f2d014d04c
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-10 16:01:24 +0200

    Create character service for API calls

---
+++ apps/front/src/services/api/characterService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { ApiClient } from "../client";
+
+export class CharacterService {
+  private axios;
+
+  constructor(axios: ApiClient) {
+    this.axios = axios.instance;
+  }
+
+  public async delete(userId: string, characterId: string) {
+    try {
+      const response = await this.axios.delete(
+        `/user/${userId}/character/${characterId}`,
+        { withCredentials: true },
+      );
+      return response;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 400) {
+          throw new Error("Cette action est impossible.");
+        } else if (error.response?.status === 404) {
+          throw new Error("Cette évènement n'existe plus.");
+        }
+      }
+      throw error;
+    }
+  }
+}

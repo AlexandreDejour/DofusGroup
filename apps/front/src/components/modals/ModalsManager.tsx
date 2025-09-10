e80c54f841705f12bdf0524447fc28cbfd72cc70
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-10 18:31:30 +0200

    Import UpdateForm in ModalManager

--- apps/front/src/components/modals/ModalsManager.tsx
+++ apps/front/src/components/modals/ModalsManager.tsx
@@ -4,13 +4,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import { useModal } from "../../contexts/modalContext";
-import RegisterForm from "./RegisterForm/RegisterForm";
-import LoginForm from "./LoginForm/LoginForm";
+
+import LoginForm from "./Forms/LoginForm";
+import UpdateForm from "./Forms/UpdateForm";
+import RegisterForm from "./Forms/RegisterForm";
 
 export default function ModalsManager() {
   const { isOpen, modalType, handleSubmit, closeModal } = useModal();
 
-  if (!isOpen) return null;
+  if (!isOpen || !modalType) return null;
 
   return (
     <div className="modal" onClick={closeModal} role="dialog">
@@ -31,6 +33,10 @@ export default function ModalsManager() {
 
           {modalType === "login" && (
             <LoginForm handleSubmit={(event) => handleSubmit(event)} />
+          )}
+
+          {["mail", "password", "username"].includes(modalType) && (
+            <UpdateForm field={modalType} handleSubmit={handleSubmit} />
           )}
         </div>
       </div>

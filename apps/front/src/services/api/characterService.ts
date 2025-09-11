02f088c89cc953a5976648aa86c8e33946db55b3
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-11 15:57:18 +0200

    Create characterService's create method

--- apps/front/src/services/api/characterService.ts
+++ apps/front/src/services/api/characterService.ts
@@ -1,11 +1,36 @@
 import axios from "axios";
 import { ApiClient } from "../client";
+import { Character } from "../../types/character";
+import { CreateCharacterForm } from "../../types/form";
 
 export class CharacterService {
   private axios;
 
   constructor(axios: ApiClient) {
     this.axios = axios.instance;
+  }
+
+  public async create(
+    userId: string,
+    data: CreateCharacterForm,
+  ): Promise<Character> {
+    try {
+      const response = await this.axios.post<Character>(
+        `/user/${userId}/character`,
+        data,
+        { withCredentials: true },
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 400) {
+          throw new Error(
+            "Vous devez être connecter pour créer un personnage.",
+          );
+        }
+      }
+      throw error;
+    }
   }
 
   public async delete(userId: string, characterId: string) {

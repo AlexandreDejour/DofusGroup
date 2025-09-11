0073141ed9123fb42208abfb93b8c89917d7a52c
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-11 15:59:12 +0200

    Update FormDataToObject function to treat number and boolean

--- apps/front/src/contexts/utils/formDataToObject.ts
+++ apps/front/src/contexts/utils/formDataToObject.ts
@@ -1,14 +1,23 @@
 import DOMPurify from "dompurify";
 
-export default function formDataToObject<T extends Record<string, string>>(
-  formData: FormData,
-  keys: (keyof T)[],
-): T {
+export default function formDataToObject<
+  T extends Record<string, string | number | boolean>,
+>(formData: FormData, keys: (keyof T)[]): T {
   const obj = Object.fromEntries(formData) as Record<string, string>;
-  const result: Record<string, string> = {};
+  const result: Partial<T> = {};
 
   keys.forEach((key) => {
-    result[key as string] = DOMPurify.sanitize(obj[key as string] || "");
+    const raw = obj[key as string] ?? "";
+
+    const sanitized = DOMPurify.sanitize(raw);
+
+    if (typeof ({} as T)[key] === "number") {
+      (result as any)[key] = Number(sanitized);
+    } else if (typeof ({} as T)[key] === "boolean") {
+      (result as any)[key] = sanitized === "true";
+    } else {
+      (result as any)[key] = sanitized;
+    }
   });
 
   return result as T;

0af2eac59de13951cef2b87c84b911c74aa81702
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-13 16:32:13 +0200

    Update generateOptions to support new inputs

--- apps/front/src/components/modals/utils/generateOptions.ts
+++ apps/front/src/components/modals/utils/generateOptions.ts
@@ -1,18 +1,35 @@
+import { Area, Dungeon, SubArea } from "../../../types/dofusDB";
 import { Breed } from "../../../types/breed";
 import { Server } from "../../../types/server";
+import { Alignment } from "../../../types/alignment";
+import { Tag } from "../../../types/tag";
+import { Status } from "../../../types/status";
+import { Character } from "../../../types/character";
 
-interface ServerOptions {
+export type BaseOptions = {
   value: string;
   label: string;
-}
+};
 
-interface BreedOption extends ServerOptions {
+type ServerOptions = BaseOptions;
+type AlignmentOptions = BaseOptions;
+type AreaOptions = BaseOptions;
+type SubAreaOptions = BaseOptions;
+type DungeonOptions = BaseOptions;
+type TagOptions = BaseOptions;
+type StatusOptions = BaseOptions;
+type CharacterOptions = BaseOptions;
+
+type BreedOption = ServerOptions & {
   imgSrc: string;
-}
+};
 
 export const generateOptions = {
   servers: (servers: Server[]): ServerOptions[] =>
     servers.map((s) => ({ value: s.id, label: s.name })),
+
+  alignments: (alignments: Alignment[]): AlignmentOptions[] =>
+    alignments.map((a) => ({ value: a.name, label: a.name })),
 
   breeds: (breeds: Breed[], sex: string): BreedOption[] => {
     const completeSex = sex === "F" ? "female" : "male";
@@ -23,4 +40,22 @@ export const generateOptions = {
       imgSrc: `/miniatures/${b.name.toLowerCase()}_${completeSex}.webp`,
     }));
   },
+
+  tags: (tags: Tag[]): TagOptions[] =>
+    tags.map((t) => ({ value: t.id, label: t.name })),
+
+  characters: (characters: Character[]): CharacterOptions[] =>
+    characters.map((c) => ({ value: c.id, label: c.name })),
+
+  areas: (areas: Area[]): AreaOptions[] =>
+    areas.map((a) => ({ value: a.name.fr, label: a.name.fr })),
+
+  subAreas: (subAreas: SubArea[]): SubAreaOptions[] =>
+    subAreas.map((s) => ({ value: s.name.fr, label: s.name.fr })),
+
+  dungeons: (dungeons: Dungeon[]): DungeonOptions[] =>
+    dungeons.map((d) => ({ value: d.name.fr, label: d.name.fr })),
+
+  statutes: (statutes: Status[]): StatusOptions[] =>
+    statutes.map((s) => ({ value: s.value, label: s.label })),
 };

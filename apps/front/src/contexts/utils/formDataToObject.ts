8ee6e7d416e487bb2686ac4f007dfe163b0d6df9
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-13 16:32:37 +0200

    Update formDataToObject to support new inputs

--- apps/front/src/contexts/utils/formDataToObject.ts
+++ apps/front/src/contexts/utils/formDataToObject.ts
@@ -1,12 +1,17 @@
 import DOMPurify from "dompurify";
 
 export default function formDataToObject<
-  T extends Record<string, string | number | boolean | undefined>,
+  T extends Record<
+    string,
+    string | string[] | number | boolean | Date | undefined
+  >,
 >(
   formData: FormData,
   keys: (keyof T)[],
   booleanKeys: (keyof T)[] = [],
   numberKeys: (keyof T)[] = [],
+  dateKeys: (keyof T)[] = [],
+  arrayKeys: (keyof T)[] = [],
 ): T {
   const obj = Object.fromEntries(formData) as Record<string, string>;
   const result: Partial<T> = {};
@@ -21,6 +26,18 @@ export default function formDataToObject<
         (result as any)[key] = Number(DOMPurify.sanitize(raw));
       }
       // else, don't add key
+    } else if (dateKeys.includes(key)) {
+      const raw = obj[key as string];
+      if (raw !== undefined && raw !== "") {
+        // on sanitize puis on crée un Date
+        const safe = DOMPurify.sanitize(raw);
+        (result as any)[key] = new Date(safe);
+      }
+    } else if (arrayKeys.includes(key)) {
+      const rawValues = formData.getAll(key as string);
+      (result as any)[key] = rawValues
+        .map((val) => DOMPurify.sanitize(val as string))
+        .filter((val) => val !== ""); // clean & ignore vides
     } else {
       const raw = obj[key as string];
       if (raw !== undefined && raw !== "") {

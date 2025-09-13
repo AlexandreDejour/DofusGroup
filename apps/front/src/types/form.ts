660af0732417a3d6a95b3a123f67962f12163e43
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-13 16:27:35 +0200

    Create new type for event form creation

--- apps/front/src/types/form.ts
+++ apps/front/src/types/form.ts
@@ -19,3 +19,18 @@ export type CreateCharacterForm = {
   breed_id: string;
   server_id: string;
 };
+
+export type CreateEventForm = {
+  title: string;
+  date: Date;
+  duration?: number;
+  area?: string;
+  sub_area?: string;
+  donjon_name?: string;
+  description?: string;
+  max_players: number;
+  status: string;
+  tag_id: string;
+  server_id: string;
+  characters_id: string[];
+};

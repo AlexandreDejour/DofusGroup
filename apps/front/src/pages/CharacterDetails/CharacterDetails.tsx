f21d49ba76f5d74056bf2fb97761c2fe1505a382
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-16 13:09:52 +0200

    Build character details page

--- apps/front/src/pages/CharacterDetails/CharacterDetails.tsx
+++ apps/front/src/pages/CharacterDetails/CharacterDetails.tsx
@@ -17,6 +17,7 @@ export default function CharacterDetails() {
   const { id } = useParams();
   const { user } = useAuth();
 
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [character, setCharacter] = useState<CharacterEnriched | null>(null);
 
   if (!id) return <Navigate to="/not-found" replace />;
@@ -33,14 +34,58 @@ export default function CharacterDetails() {
         } else if (error instanceof Error) {
           console.error("General error:", error.message);
         }
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchCharacter();
-  });
+  }, [id]);
+
+  if (!isLoading && character === null)
+    return <Navigate to="/not-found" replace />;
 
   return (
     <main className="character">
-      {character ? <p>{character.name}</p> : <p>Chargement en cours</p>}
+      {character ? (
+        <section>
+          {character.sex === "M" ? (
+            <img
+              className="character_img"
+              src={`/characters/${character.breed.name.toLocaleLowerCase()}_male.webp`}
+              alt={`Miniature de classe ${character.breed.name.toLocaleLowerCase()}`}
+            />
+          ) : (
+            <img
+              className="character_img"
+              src={`/characters/${character.breed.name.toLocaleLowerCase()}_female.webp`}
+              alt={`Miniature de classe ${character.breed.name.toLocaleLowerCase()}`}
+            />
+          )}
+
+          <div className="character_details">
+            <p>Pseudo: {character.name}</p>
+            <p>Classe: {character.breed.name}</p>
+            <p>Niveau: {character.level}</p>
+            <p>Serveur: {character.server.name}</p>
+            <p>Sexe: {character.sex}</p>
+            <p>Alignement: {character.alignment}</p>
+            <a href={character.stuff}>Stuff: {character.stuff}</a>
+          </div>
+
+          {character.user.id === user?.id ? (
+            <div className="character_buttons">
+              <button type="button" className="button">
+                Modifier
+              </button>
+              <button type="button" className="button delete">
+                Supprimer
+              </button>
+            </div>
+          ) : null}
+        </section>
+      ) : (
+        <p>Chargement en cours</p>
+      )}
     </main>
   );
 }

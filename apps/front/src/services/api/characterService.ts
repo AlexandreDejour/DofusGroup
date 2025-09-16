33ae94c7e762d143f94b7f3c3b9319c06c8368b7
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-16 16:56:21 +0200

    Create characterService's update method

--- apps/front/src/services/api/characterService.ts
+++ apps/front/src/services/api/characterService.ts
@@ -68,6 +68,54 @@ export class CharacterService {
         data,
         { withCredentials: true },
       );
+
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 400) {
+          throw new Error(
+            "Les informations transmises sont érronées ou incomplètes.",
+          );
+        }
+
+        if (error.response?.status === 401) {
+          throw new Error(
+            "Vous devez être connecter pour créer un personnage.",
+          );
+        }
+
+        if (error.response?.status === 403) {
+          throw new Error(
+            "La création de personnage est réservée à votre compte.",
+          );
+        }
+      }
+      throw error;
+    }
+  }
+
+  public async update(
+    userId: string,
+    characterId: string,
+    data: CreateCharacterForm,
+  ): Promise<CharacterEnriched> {
+    if (!(data.level >= 1 && data.level <= 200)) {
+      throw new Error(
+        "Le niveau de votre personnage doit être compris entre 1 et 200.",
+      );
+    }
+
+    if (data.stuff && !this.urlRegex.test(data.stuff)) {
+      throw new Error("Seules les URL provenant de DofusBook sont acceptées.");
+    }
+
+    try {
+      const response = await this.axios.patch<CharacterEnriched>(
+        `/user/${userId}/character/${characterId}`,
+        data,
+        { withCredentials: true },
+      );
+
       return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {

19b3996c3795f01c85266512c5f08cfeb6b12ec1
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-17 17:07:29 +0200

    Create dedicaded characterCard for event details

---
+++ apps/front/src/components/EventCharacterCard/EventCharacterCard.tsx
@@ -0,0 +1,58 @@
+import "./EventCharacterCard.scss";
+
+import { useNavigate } from "react-router";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+import { CharacterEnriched } from "../../types/character";
+import { TargetType } from "../../contexts/modalContext";
+
+interface CharacterCardProps {
+  character: CharacterEnriched;
+  handleDelete: (targetType: TargetType, targetId: string) => Promise<void>;
+}
+
+export default function EventCharacterCard({
+  character,
+  handleDelete,
+}: CharacterCardProps) {
+  const navigate = useNavigate();
+
+  return (
+    <article className="event_character_card">
+      {character.sex === "M" ? (
+        <img
+          className="event_character_card_img"
+          src={`/miniatures/${character.breed.name.toLocaleLowerCase()}_male.webp`}
+          alt={`Miniature de classe ${character.breed.name.toLocaleLowerCase()}`}
+        />
+      ) : (
+        <img
+          className="event_character_card_img"
+          src={`/miniatures/${character.breed.name.toLocaleLowerCase()}_female.webp`}
+          alt={`Miniature de classe ${character.breed.name.toLocaleLowerCase()}`}
+        />
+      )}
+
+      <h3 className="event_character_card_title">{character.name}</h3>
+      <p className="event_character_card_breed">{character.breed.name}</p>
+      <p className="event_character_card_level">niveau: {character.level}</p>
+      <div className="event_character_card_buttons">
+        <button
+          className="event_character_card_buttons_details button"
+          onClick={() => navigate(`/character/${character.id}`)}
+        >
+          Détails
+        </button>
+        <button
+          className="event_character_card_buttons_delete button delete"
+          aria-label={`Delete event ${character.name}`}
+          onClick={() => handleDelete("character", character.id)}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </div>
+    </article>
+  );
+}

9ed04f9631c3d2563231acecc1abf7c8cf3ee5de
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-18 14:47:27 +0200

    Create character checkbox component

---
+++ apps/front/src/components/modals/FormComponents/Checkbox/CharactersCheckbox.tsx
@@ -0,0 +1,54 @@
+import "./CharactersCheckbox.scss";
+
+import { CharacterEnriched } from "../../../../types/character";
+
+interface CharacterCheckboxProps {
+  characters: CharacterEnriched[];
+}
+
+export default function CharactersCheckbox({
+  characters,
+}: CharacterCheckboxProps) {
+  return (
+    <fieldset className="character">
+      <legend>Vos personnages:</legend>
+      <div className="character_choices">
+        {characters.map((character) => (
+          <label
+            key={character.id}
+            htmlFor="characters_id"
+            className="character_choices_label"
+          >
+            <div className="character_choices_label_card">
+              {character.sex === "M" ? (
+                <img
+                  className="character_choices_label_card_img"
+                  src={`/miniatures/${character.breed.name.toLocaleLowerCase()}_male.webp`}
+                  alt={`Miniature de classe ${character.breed.name.toLocaleLowerCase()}`}
+                />
+              ) : (
+                <img
+                  className="character_choices_label_card_img"
+                  src={`/miniatures/${character.breed.name.toLocaleLowerCase()}_female.webp`}
+                  alt={`Miniature de classe ${character.breed.name.toLocaleLowerCase()}`}
+                />
+              )}
+              <div className="character_choices_label_card_details">
+                <h4 className="character_choices_label_card_details_title">
+                  {character.name}
+                </h4>
+                <p className="character_choices_label_card_details_breed">
+                  {character.breed.name}
+                </p>
+                <p className="character_choices_label_card_details_level">
+                  niveau: {character.level}
+                </p>
+              </div>
+            </div>
+            <input type="checkbox" name="characters_id" value={character.id} />
+          </label>
+        ))}
+      </div>
+    </fieldset>
+  );
+}

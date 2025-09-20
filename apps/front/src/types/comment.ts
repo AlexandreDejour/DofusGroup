030a5c7b81ec2151bf9ce4793d45af39af3e9c7f
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-21 00:48:16 +0200

    Update comment types

--- apps/front/src/types/comment.ts
+++ apps/front/src/types/comment.ts
@@ -1,4 +1,12 @@
+import { User } from "./user";
+import { Event } from "./event";
+
 export type Comment = {
   id: string;
   content: string;
+};
+
+export type CommentEnriched = Comment & {
+  user?: User;
+  event?: Event;
 };

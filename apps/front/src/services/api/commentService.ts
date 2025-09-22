663138cfc02e5a3caa6506e24c23ff3fab132ca1
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-22 15:00:16 +0200

    Add new update method

--- apps/front/src/services/api/commentService.ts
+++ apps/front/src/services/api/commentService.ts
@@ -42,4 +42,39 @@ export class CommentService {
       throw error;
     }
   }
+
+  public async update(
+    userId: string,
+    commentId: string,
+    data: CreateCommentForm,
+  ): Promise<Comment> {
+    try {
+      const response = await this.axios.patch<Comment>(
+        `/user/${userId}/comment/${commentId}`,
+        data,
+        { withCredentials: true },
+      );
+
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          throw new Error(
+            "Vous devez être connecter pour créer un personnage.",
+          );
+        }
+
+        if (error.response?.status === 403) {
+          throw new Error(
+            "La création de personnage est réservée à votre compte.",
+          );
+        }
+
+        if (error.response?.status === 404) {
+          throw new Error("Ce commentaire n'existe plus.");
+        }
+      }
+      throw error;
+    }
+  }
 }

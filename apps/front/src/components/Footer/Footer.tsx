93fa388f130bf23b9a7cbec49ff7ab59eb995a82
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-23 16:54:48 +0200

    Update Footer and style sheet

--- apps/front/src/components/Footer/Footer.tsx
+++ apps/front/src/components/Footer/Footer.tsx
@@ -7,73 +7,83 @@ export default function Footer() {
   const { user } = useAuth();
 
   return (
-    <footer>
+    <footer className="footer">
       <div className="footer_container">
-        <h3>DofusGroup</h3>
-        <ul>
-          <li className="link">
-            <Link to="/">Évènements</Link>
-          </li>
-          {user ? (
+        <div className="footer_container_list">
+          <h3>DofusGroup</h3>
+          <ul>
             <li className="link">
-              <Link to="/profile">Profil</Link>
+              <Link to="/">Évènements</Link>
             </li>
-          ) : null}
-          <li className="link">
-            <Link to="/about">À propos</Link>
-          </li>
-        </ul>
+            {user ? (
+              <li className="link">
+                <Link to="/profile">Profil</Link>
+              </li>
+            ) : null}
+            <li className="link">
+              <Link to="/about">À propos</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer_container_list">
+          <h3>Autres liens</h3>
+          <ul>
+            <li>
+              <Link to="/gcu">Conditions générales d'utilisation</Link>
+            </li>
+            <li>
+              <Link to="/privacy_policy">Politique de confidentialité</Link>
+            </li>
+            <li>
+              <a href="#">Nous contacter</a>
+            </li>
+            <li>
+              <a href="#">Signaler un bug</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer_container_list">
+          <h3>Sites utiles</h3>
+          <ul>
+            <li>
+              <a
+                href="https://www.dofus.com/fr"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Dofus
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.dofusbook.net/fr/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                DofusBook
+              </a>
+            </li>
+            <li>
+              <a href="https://dofusdb.fr/fr/" target="_blank" rel="noreferrer">
+                DofusDB
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.dofuspourlesnoobs.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Dofus pour les noobs
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="footer_container">
-        <h3>Autres liens</h3>
-        <ul>
-          <li>
-            <Link to="/gcu">Conditions générales d'utilisation</Link>
-          </li>
-          <li>
-            <Link to="/confidentiality">Politique de confidentialité</Link>
-          </li>
-          <li>
-            <a href="#">Nous contacter</a>
-          </li>
-          <li>
-            <a href="#">Signaler un bug</a>
-          </li>
-        </ul>
-      </div>
-      <div className="footer_container">
-        <h3>Sites utiles</h3>
-        <ul>
-          <li>
-            <a href="https://www.dofus.com/fr" target="_blank" rel="noreferrer">
-              Dofus
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.dofusbook.net/fr/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              DofusBook
-            </a>
-          </li>
-          <li>
-            <a href="https://dofusdb.fr/fr/" target="_blank" rel="noreferrer">
-              DofusDB
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.dofuspourlesnoobs.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Dofus pour les noobs
-            </a>
-          </li>
-        </ul>
-      </div>
+
+      <p>
+        &copy; {new Date().getFullYear()} DofusGroup — Tous droits réservés.
+      </p>
     </footer>
   );
 }

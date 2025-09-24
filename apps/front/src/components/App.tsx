33808f3d6e71220cd0a7fcf08a072f29eaea1316
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-24 11:46:33 +0200

    Import about page in App

--- apps/front/src/components/App.tsx
+++ apps/front/src/components/App.tsx
@@ -2,16 +2,17 @@ import "./App.scss";
 
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home/Home";
+import GCU from "../pages/GCU/GCU";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
 import Profile from "../pages/Profile/Profile";
 import ModalsManager from "./modals/ModalsManager";
 import EventDetails from "../pages/EventDetails/EventDetails";
+import PrivacyPolicy from "../pages/PrivacyPolicy/PrivacyPolicy";
 import NotificationContainer from "./Notification/NotificationContainer";
 import CharacterDetails from "../pages/CharacterDetails/CharacterDetails";
-import PrivacyPolicy from "../pages/PrivacyPolicy/PrivacyPolicy";
-import GCU from "../pages/GCU/GCU";
 
 export default function App() {
   return (
@@ -24,6 +25,7 @@ export default function App() {
         <Route path="/event/:id" element={<EventDetails />} />
         <Route path="/character/:id" element={<CharacterDetails />} />
         <Route path="/privacy_policy" element={<PrivacyPolicy />} />
+        <Route path="/about" element={<About />} />
         <Route path="/gcu" element={<GCU />} />
       </Routes>
       <NotificationContainer />

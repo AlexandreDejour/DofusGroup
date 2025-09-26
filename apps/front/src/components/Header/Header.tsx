ecdc0967b482aedad9c5104048aa8bd8ef6c1378
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-26 12:41:21 +0200

    Update header style sheet for responsive mobile design

--- apps/front/src/components/Header/Header.tsx
+++ apps/front/src/components/Header/Header.tsx
@@ -4,24 +4,35 @@ import { Link } from "react-router-dom";
 
 import { useAuth } from "../../contexts/authContext";
 import { useModal } from "../../contexts/modalContext";
+import { useScreen } from "../../contexts/screenContext";
 
 export default function Header() {
-  const { user, logout } = useAuth();
+  const { isDesktop } = useScreen();
   const { openModal } = useModal();
+  const { user, logout } = useAuth();
 
   return (
     <header className="header">
-      <div className="header_logo">
-        <h1>DofusGroup</h1>
-      </div>
+      {isDesktop ? (
+        <div className="header_logo">
+          <h1>DofusGroup</h1>
+        </div>
+      ) : (
+        <div className="header_container">
+          <div className="header_container_logo"></div>
+          <h1>DofusGroup</h1>
+        </div>
+      )}
       <nav className="header_nav">
         <ul className="header_nav_list">
           <li className="header_nav_list_item nav_link link">
             <Link to="/">Évènements</Link>
           </li>
-          <li className="header_nav_list_item nav_link link">
-            <Link to="/about">À propos</Link>
-          </li>
+          {isDesktop && (
+            <li className="header_nav_list_item nav_link link">
+              <Link to="/about">À propos</Link>
+            </li>
+          )}
           {user ? (
             <>
               <li className="header_nav_list_item nav_link link">

7f80eceb7db1aeaafd83c3dcacd77d383adeb3cf
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-09-29 14:20:53 +0200

    Use i18n in header and footer

--- apps/front/src/components/Footer/Footer.tsx
+++ apps/front/src/components/Footer/Footer.tsx
@@ -1,10 +1,18 @@
-import { Link } from "react-router";
-
 import "./Footer.scss";
+
+import { Link } from "react-router";
+import ReactCountryFlag from "react-country-flag";
+
 import { useAuth } from "../../contexts/authContext";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   const { user } = useAuth();
+  const { t, i18n } = useTranslation();
 
   return (
     <footer className="footer">
@@ -14,31 +22,31 @@ export default function Footer() {
           <ul>
             {user ? (
               <li>
-                <Link to="/profile">Profil</Link>
+                <Link to="/profile">{t("profile")}</Link>
               </li>
             ) : null}
             <li>
-              <Link to="/about">À propos</Link>
+              <Link to="/about">{t("about")}</Link>
             </li>
             <li>
-              <Link to="/">Évènements</Link>
+              <Link to="/">{t("events")}</Link>
             </li>
           </ul>
         </div>
         <div className="footer_container_list">
-          <h3>Autres liens</h3>
+          <h3>{t("otherLinks")}</h3>
           <ul>
             <li>
-              <a href="#">Signaler un bug</a>
+              <a href="#">{t("reportBug")}</a>
             </li>
             <li>
-              <a href="#">Nous contacter</a>
+              <a href="#">{t("contactUs")}</a>
             </li>
             <li>
-              <Link to="/privacy_policy">Politique de confidentialité</Link>
+              <Link to="/privacy_policy">{t("privacyPolicy")}</Link>
             </li>
             <li>
-              <Link to="/gcu">Conditions générales d'utilisation</Link>
+              <Link to="/gcu">{t("gcu")}</Link>
             </li>
           </ul>
         </div>
@@ -81,8 +89,31 @@ export default function Footer() {
         </div>
       </div>
 
+      <div className="footer_language">
+        <button
+          aria-label={t("changeLanguage", { language: t("french") })}
+          onClick={() => changeLanguage("fr")}
+        >
+          <ReactCountryFlag
+            countryCode="FR"
+            svg
+            style={{ width: "24px", height: "24px" }}
+          />
+        </button>
+        <button
+          aria-label={t("changeLanguage", { language: t("english") })}
+          onClick={() => changeLanguage("en")}
+        >
+          <ReactCountryFlag
+            countryCode="GB"
+            svg
+            style={{ width: "24px", height: "24px" }}
+          />
+        </button>
+      </div>
+
       <p>
-        &copy; {new Date().getFullYear()} DofusGroup — Tous droits réservés.
+        &copy; {new Date().getFullYear()} DofusGroup — {t("allRightsReserved")}.
       </p>
     </footer>
   );
--- apps/front/src/components/Header/Header.tsx
+++ apps/front/src/components/Header/Header.tsx
@@ -5,11 +5,13 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/authContext";
 import { useModal } from "../../contexts/modalContext";
 import { useScreen } from "../../contexts/screenContext";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
   const { isDesktop } = useScreen();
   const { openModal } = useModal();
   const { user, logout } = useAuth();
+  const { t } = useTranslation();
 
   return (
     <header className="header">
@@ -26,17 +28,17 @@ export default function Header() {
       <nav className="header_nav">
         <ul className="header_nav_list">
           <li className="header_nav_list_item nav_link link">
-            <Link to="/">Évènements</Link>
+            <Link to="/">{t("events")}</Link>
           </li>
           {isDesktop && (
             <li className="header_nav_list_item nav_link link">
-              <Link to="/about">À propos</Link>
+              <Link to="/about">{t("about")}</Link>
             </li>
           )}
           {user ? (
             <>
               <li className="header_nav_list_item nav_link link">
-                <Link to="/profile">Profil</Link>
+                <Link to="/profile">{t("profile")}</Link>
               </li>
               <li className="header_nav_list_item">
                 <button
@@ -44,7 +46,7 @@ export default function Header() {
                   onClick={() => logout()}
                   className="nav_button button"
                 >
-                  Déconnexion
+                  {t("logout")}
                 </button>
               </li>
             </>
@@ -56,7 +58,7 @@ export default function Header() {
                   onClick={() => openModal("login")}
                   className="nav_button button"
                 >
-                  Connexion
+                  {t("login")}
                 </button>
               </li>
               <li className="header_nav_list_item">
@@ -65,7 +67,7 @@ export default function Header() {
                   onClick={() => openModal("register")}
                   className="nav_button button"
                 >
-                  Inscription
+                  {t("register")}
                 </button>
               </li>
             </>

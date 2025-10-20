4eaeaf9383adbb50295e84678b69b056648e8028
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-10-20 11:45:45 +0200

    Add environment variable

--- apps/back/src/config/config.ts
+++ apps/back/src/config/config.ts
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV !== "production") {
 export class Config {
   private static instance: Config;
 
+  readonly environment: string;
   readonly port: number;
   readonly baseUrl: string;
   readonly pgUrl: string;
@@ -16,6 +17,7 @@ export class Config {
   readonly jwtSecret: string;
 
   private constructor() {
+    this.environment = process.env.NODE_ENV as string;
     this.port = Number(process.env.PORT);
     this.baseUrl = process.env.BASE_URL as string;
     this.pgUrl = process.env.PG_URL as string;
@@ -33,6 +35,7 @@ export class Config {
   }
 
   private static check(config: Config) {
+    if (!config.environment) throw new Error("NODE_ENV is required");
     if (!config.port || isNaN(config.port))
       throw new Error("PORT is required and must be a number");
     if (!config.baseUrl) throw new Error("BASE_URL is required");

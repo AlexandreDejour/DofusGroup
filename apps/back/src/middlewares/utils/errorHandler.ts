07a4365676f87235ab461a38fe124e010897bd65
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-10-21 17:23:45 +0200

    Only use logger in production

--- apps/back/src/middlewares/utils/errorHandler.ts
+++ apps/back/src/middlewares/utils/errorHandler.ts
@@ -2,7 +2,10 @@ import { Request, Response, NextFunction } from "express";
 import status from "http-status";
 import createHttpError, { HttpError } from "http-errors";
 
+import { Config } from "../../config/config.js";
 import logger from "./logger.js";
+
+const config = Config.getInstance();
 
 export function notFound(_req: Request, _res: Response, next: NextFunction) {
   next(createHttpError(status.NOT_FOUND, "Not Found"));
@@ -18,9 +21,11 @@ export function errorHandler(
     error.status || error.statusCode || status.INTERNAL_SERVER_ERROR;
   console.error(error);
 
-  if (statusCode < 500) {
-    logger.warn(`${statusCode} - ${error.message}`);
-  } else logger.error(`Error: ${error.message}`, { stack: error.stack });
+  if (config.environment === "production") {
+    if (statusCode < 500) {
+      logger.warn(`${statusCode} - ${error.message}`);
+    } else logger.error(`Error: ${error.message}`, { stack: error.stack });
+  }
 
   res.status(statusCode).json({
     message: error.message || "Internal Server Error",
--- apps/back/src/middlewares/utils/logger.ts
+++ apps/back/src/middlewares/utils/logger.ts
@@ -4,34 +4,50 @@ import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
 
+import { Config } from "../../config/config.js";
+
+const config = Config.getInstance();
+
 // log folder
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const logDir = path.join(__dirname, "../../../logs");
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir, { recursive: true });
+const transports: winston.transport[] = [
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple(),
+    ),
+  }),
+];
+
+if (config.environment === "production") {
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+  }
+
+  transports.push(
+    new winston.transports.DailyRotateFile({
+      dirname: logDir,
+      filename: "warn-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "14d",
+      level: "warn",
+    }),
+    new winston.transports.DailyRotateFile({
+      dirname: logDir,
+      filename: "error-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+      level: "error",
+    }),
+  );
 }
-
-const infoTransport = new winston.transports.DailyRotateFile({
-  dirname: logDir,
-  filename: "warn-%DATE%.log",
-  datePattern: "YYYY-MM-DD",
-  zippedArchive: true,
-  maxSize: "20m",
-  maxFiles: "14d",
-  level: "warn",
-});
-
-const errorTransport = new winston.transports.DailyRotateFile({
-  dirname: logDir,
-  filename: "error-%DATE%.log",
-  datePattern: "YYYY-MM-DD",
-  zippedArchive: true,
-  maxSize: "20m",
-  maxFiles: "30d",
-  level: "error",
-});
 
 const logger = winston.createLogger({
   level: "info",
@@ -40,16 +56,7 @@ const logger = winston.createLogger({
     winston.format.errors({ stack: true }),
     winston.format.json(),
   ),
-  transports: [
-    infoTransport,
-    errorTransport,
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
-      ),
-    }),
-  ],
+  transports,
 });
 
 export default logger;

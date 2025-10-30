79f00bc439a1c2fc5389972d45aea6e784253069
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-10-30 15:53:25 +0100

    Increase request limiter limit

--- apps/back/src/middlewares/utils/requestLimiter.ts
+++ apps/back/src/middlewares/utils/requestLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const requestLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Max 5 attemps
+  max: 10, // Max 10 attemps
   message: { error: "Too much attemps, try again later." },
   standardHeaders: true,
   legacyHeaders: false,

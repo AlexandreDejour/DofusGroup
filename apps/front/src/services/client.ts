938e40f8a28e211222d19b42053ad9d240401301
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-10-30 14:54:59 +0100

    Import exios interceptor

--- apps/front/src/services/client.ts
+++ apps/front/src/services/client.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosInstance } from "axios";
 
+import axiosInterceptor from "./utils/axiosInterceptor";
+
 export class ApiClient {
   private axiosInstance: AxiosInstance;
 
   constructor(baseURL: string) {
     this.axiosInstance = axios.create({ baseURL });
+    axiosInterceptor(this.axiosInstance);
   }
 
   get instance() {

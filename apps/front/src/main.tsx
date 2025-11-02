0239f3d01f76ace2de362544c7b7e9abf58261fd
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-11-02 16:42:00 +0100

    Disable strict mode

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -26,17 +26,15 @@ i18n.on("languageChanged", (lng) => {
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ScreenProvider>
-        <NotificationProvider>
-          <AuthProvider>
-            <ModalProvider>
-              <App />
-            </ModalProvider>
-          </AuthProvider>
-        </NotificationProvider>
-      </ScreenProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ScreenProvider>
+      <NotificationProvider>
+        <AuthProvider>
+          <ModalProvider>
+            <App />
+          </ModalProvider>
+        </AuthProvider>
+      </NotificationProvider>
+    </ScreenProvider>
+  </BrowserRouter>,
 );

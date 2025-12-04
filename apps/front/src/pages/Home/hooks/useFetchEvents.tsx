562400d4e3e7bd1f1a0bc6ebede4d071e0806d56
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-12-04 17:52:16 +0100

    Update Home's unit tests

--- apps/front/src/pages/Home/hooks/useFetchEvents.tsx
+++ apps/front/src/pages/Home/hooks/useFetchEvents.tsx
@@ -39,7 +39,7 @@ export default function useFetchEvents(
     };
 
     fetchEvents();
-  }, [currentPage, setEvents, setTotalPages]);
+  }, [currentPage]);
 
   return { events, isLoading, error };
 }

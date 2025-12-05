b0757e3dffb36ad02ba586937c3f1a200c228779
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-12-05 15:58:52 +0100

    Replace useEffect by custom hooks and insert spinner

--- apps/front/src/pages/Profile/Profile.tsx
+++ apps/front/src/pages/Profile/Profile.tsx
@@ -1,95 +1,26 @@
 import "./Profile.scss";
 
-import { isAxiosError } from "axios";
-import { useNavigate } from "react-router";
-import { useEffect, useState } from "react";
 import { useTypedTranslation } from "../../i18n/i18n-helper";
 
 import { useAuth } from "../../contexts/authContext";
 import { useModal } from "../../contexts/modalContext";
-import { useNotification } from "../../contexts/notificationContext";
 
-import { Event } from "../../types/event";
-import { UserEnriched } from "../../types/user";
+import useUserEnriched from "./hooks/useFetchUserEnriched";
+import useUpComingEvents from "./hooks/useFetchIncomingEvents";
 
-import { Config } from "../../config/config";
-import { ApiClient } from "../../services/client";
-import { UserService } from "../../services/api/userService";
-import { EventService } from "../../services/api/eventService";
-
+import Spinner from "../../components/Spinner/Spinner";
 import CharacterCard from "../../components/CharacterCard/CharacterCard";
 import ProfileEventCard from "../../components/ProfileEventCard/ProfileEventCard";
 
-const config = Config.getInstance();
-const axios = new ApiClient(config.backUrl);
-const userService = new UserService(axios);
-const eventService = new EventService(axios);
-
 export default function Profile() {
-  const navigate = useNavigate();
   const t = useTypedTranslation();
 
-  const { showError } = useNotification();
-  const { user, isAuthLoading } = useAuth();
+  const { user } = useAuth();
   const { openModal, handleDelete } = useModal();
 
-  const [userEnriched, setUserEnriched] = useState<UserEnriched | null>(null);
-  const [upComingEvents, setUpcomingEvents] = useState<Event[]>([]);
-
-  useEffect(() => {
-    const fetchUserEnriched = async () => {
-      if (isAuthLoading) return;
-
-      if (!user) {
-        navigate("/", { replace: true });
-        return;
-      }
-
-      try {
-        const response = await userService.getOneEnriched(user.id);
-        setUserEnriched(response);
-      } catch (error) {
-        if (isAxiosError(error)) {
-          showError(t("system.error.default"), error.message);
-        } else if (error instanceof Error) {
-          showError(t("system.error.default"), t("system.error.occurred"));
-          console.error("General error:", error.message);
-        }
-      }
-    };
-
-    fetchUserEnriched();
-  }, [user, isAuthLoading]);
-
-  useEffect(() => {
-    const fetchUpcomingEvents = async () => {
-      if (!userEnriched || !userEnriched.characters?.length) return;
-
-      try {
-        const characterIds = userEnriched.characters.map((c) => c.id);
-
-        const response = await eventService.getRegistered(characterIds);
-
-        if (userEnriched.events?.length) {
-          const ownerEvents = userEnriched.events.map((event) => event.id);
-          const events = response.filter(
-            (event) => !ownerEvents.includes(event.id),
-          );
-
-          setUpcomingEvents(events);
-        } else setUpcomingEvents(response);
-      } catch (error) {
-        if (isAxiosError(error)) {
-          showError(t("system.error.default"), error.message);
-        } else if (error instanceof Error) {
-          showError(t("system.error.default"), t("system.error.occurred"));
-          console.error("General error:", error.message);
-        }
-      }
-    };
-
-    fetchUpcomingEvents();
-  }, [userEnriched]);
+  const { userEnriched, isLoading: userEnrichedLoading } = useUserEnriched();
+  const { upComingEvents, isLoading: upComingEventsLoading } =
+    useUpComingEvents(userEnriched);
 
   return (
     <>
@@ -169,57 +100,81 @@ export default function Profile() {
 
           <section className="profile_section">
             <h2 className="profile_section_title">{t("event.upComing")}</h2>
-            {upComingEvents && upComingEvents.length ? (
-              <ul className="profile_section_list">
-                {upComingEvents.map((event) => (
-                  <li key={event.id} className="profile_section_list_item">
-                    <ProfileEventCard event={event} />
-                  </li>
-                ))}
-              </ul>
+            {!upComingEventsLoading ? (
+              upComingEvents && upComingEvents.length ? (
+                <ul className="profile_section_list">
+                  {upComingEvents.map((event) => (
+                    <li key={event.id} className="profile_section_list_item">
+                      <ProfileEventCard event={event} />
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>{t("event.noEvent")}</p>
+              )
             ) : (
-              <p>{t("event.noEvent")}</p>
+              <Spinner
+                size={50}
+                color="#808080"
+                loading={upComingEventsLoading}
+              />
             )}
           </section>
 
           <section className="profile_section">
             <h2 className="profile_section_title">{t("event.your")}</h2>
-            {userEnriched.events && userEnriched.events.length ? (
-              <ul className="profile_section_list">
-                {userEnriched.events.map((event) => (
-                  <li key={event.id} className="profile_section_list_item">
-                    <ProfileEventCard
-                      event={event}
-                      handleDelete={handleDelete}
-                    />
-                  </li>
-                ))}
-              </ul>
+            {!userEnrichedLoading ? (
+              userEnriched.events && userEnriched.events.length ? (
+                <ul className="profile_section_list">
+                  {userEnriched.events.map((event) => (
+                    <li key={event.id} className="profile_section_list_item">
+                      <ProfileEventCard
+                        event={event}
+                        handleDelete={handleDelete}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>{t("event.noEvent")}</p>
+              )
             ) : (
-              <p>{t("event.noEvent")}</p>
+              <Spinner
+                size={50}
+                color="#808080"
+                loading={userEnrichedLoading}
+              />
             )}
           </section>
 
           <section className="profile_section">
             <h2 className="profile_section_title">{t("character.your")}</h2>
-            {userEnriched.characters && userEnriched.characters.length ? (
-              <ul className="profile_section_list">
-                {userEnriched.characters
-                  .sort((a, b) => a.name.localeCompare(b.name))
-                  .map((character) => (
-                    <li
-                      key={character.id}
-                      className="profile_section_list_item"
-                    >
-                      <CharacterCard
-                        character={character}
-                        handleDelete={handleDelete}
-                      />
-                    </li>
-                  ))}
-              </ul>
+            {!userEnrichedLoading ? (
+              userEnriched.characters && userEnriched.characters.length ? (
+                <ul className="profile_section_list">
+                  {userEnriched.characters
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((character) => (
+                      <li
+                        key={character.id}
+                        className="profile_section_list_item"
+                      >
+                        <CharacterCard
+                          character={character}
+                          handleDelete={handleDelete}
+                        />
+                      </li>
+                    ))}
+                </ul>
+              ) : (
+                <p>{t("character.noCharacter")}</p>
+              )
             ) : (
-              <p>{t("character.noCharacter")}</p>
+              <Spinner
+                size={50}
+                color="#808080"
+                loading={userEnrichedLoading}
+              />
             )}
           </section>
         </main>

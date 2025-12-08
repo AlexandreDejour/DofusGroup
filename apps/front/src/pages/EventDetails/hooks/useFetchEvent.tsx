b2c3d3630616e74ea149dd469b8369a203942657
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-12-08 14:11:55 +0100

    Create fetching event custom hook

---
+++ apps/front/src/pages/EventDetails/hooks/useFetchEvent.tsx
@@ -0,0 +1,44 @@
+import { isAxiosError } from "axios";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+import { CommentEnriched } from "../../../types/comment";
+import { Event, EventEnriched } from "../../../types/event";
+import { CharacterEnriched } from "../../../types/character";
+
+import { Config } from "../../../config/config";
+import { ApiClient } from "../../../services/client";
+import { EventService } from "../../../services/api/eventService";
+
+const config = Config.getInstance();
+const axios = new ApiClient(config.backUrl);
+const eventService = new EventService(axios);
+
+export default function useFetchEvent(
+  id: string,
+  updateTarget: Event | CharacterEnriched | CommentEnriched | null,
+  setEvent: Dispatch<SetStateAction<EventEnriched | null>>,
+) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await eventService.getOneEnriched(id);
+
+        setEvent(response);
+      } catch (error) {
+        if (isAxiosError(error)) setError(error.message);
+        else if (error instanceof Error) setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchEvent();
+  }, [id, updateTarget]);
+
+  return { event, isLoading, error };
+}

e51e2acb63a03fc7e15b3f0d92227fdbe30b00bb
AlexandreDejour <alexandre.dejour1@gmail.com>
2025-12-22 12:38:06 +0100

    Card becomes link to event, delete details button and add hover border

--- apps/front/src/components/EventCard/EventCard.tsx
+++ apps/front/src/components/EventCard/EventCard.tsx
@@ -17,60 +17,58 @@ export default function EventCard({ event }: EventCardProps) {
   const { isDesktop } = useScreen();
 
   return (
-    <article className="event_card">
-      <h2 className="event_card_title">
-        {event.title.charAt(0).toLocaleUpperCase() + event.title.slice(1)}
-      </h2>
-      <p
-        className="event_card_tag"
-        style={{ backgroundColor: event.tag.color }}
+    <article>
+      <a
+        href={`/event/${event.id}`}
+        className="event_card"
+        aria-label={event.title}
       >
-        {event.tag.name}
-      </p>
-
-      {isDesktop ? (
-        <>
-          <p className="event_card_server">{event.server.name}</p>
-          <p className="event_card_date">
-            {new Date(event.date).toLocaleString(undefined, {
-              dateStyle: "short",
-              timeStyle: "short",
-            })}
-          </p>
-          <p className="event_card_duration">{event.duration} min</p>
-          <p className="event_card_players">
-            {event.characters.length}/{event.max_players}
-          </p>
-        </>
-      ) : (
-        <div className="event_card_container">
-          <p className="event_card_container_server">
-            <span>Serveur:</span> {event.server.name}
-          </p>
-          <p className="event_card_container_date">
-            <span>{t("common.date")}:</span>{" "}
-            {new Date(event.date).toLocaleString(undefined, {
-              dateStyle: "short",
-              timeStyle: "short",
-            })}
-          </p>
-          <p className="event_card_container_duration">
-            {" "}
-            <span>{t("common.duration")}:</span> {event.duration} min
-          </p>
-          <p className="event_card_container_players">
-            <span>{t("common.players")}:</span> {event.characters.length}/
-            {event.max_players}
-          </p>
-        </div>
-      )}
-
-      <button
-        className="event_card_button button"
-        onClick={() => navigate(`/event/${event.id}`)}
-      >
-        {t("common.details")}
-      </button>
+        <h2 className="event_card_title">
+          {event.title.charAt(0).toLocaleUpperCase() + event.title.slice(1)}
+        </h2>
+        <p
+          className="event_card_tag"
+          style={{ backgroundColor: event.tag.color }}
+        >
+          {event.tag.name}
+        </p>
+        {isDesktop ? (
+          <>
+            <p className="event_card_server">{event.server.name}</p>
+            <p className="event_card_date">
+              {new Date(event.date).toLocaleString(undefined, {
+                dateStyle: "short",
+                timeStyle: "short",
+              })}
+            </p>
+            <p className="event_card_duration">{event.duration} min</p>
+            <p className="event_card_players">
+              {event.characters.length}/{event.max_players}
+            </p>
+          </>
+        ) : (
+          <div className="event_card_container">
+            <p className="event_card_container_server">
+              <span>Serveur:</span> {event.server.name}
+            </p>
+            <p className="event_card_container_date">
+              <span>{t("common.date")}:</span>{" "}
+              {new Date(event.date).toLocaleString(undefined, {
+                dateStyle: "short",
+                timeStyle: "short",
+              })}
+            </p>
+            <p className="event_card_container_duration">
+              {" "}
+              <span>{t("common.duration")}:</span> {event.duration} min
+            </p>
+            <p className="event_card_container_players">
+              <span>{t("common.players")}:</span> {event.characters.length}/
+              {event.max_players}
+            </p>
+          </div>
+        )}
+      </a>
     </article>
   );
 }
